4d0006f2ec530c3fbbb6a5c22e82268c471ef0a9
AkshayLalsare <akshaylalsare9@gmail.com>
2022-02-18 18:39:51 +0530

    test3 changed

--- src/test/java/com/mavengit/gittest/test3.java
+++ src/test/java/com/mavengit/gittest/test3.java
@@ -2,7 +2,11 @@ package com.mavengit.gittest;
 
 public class test3 {
 	
+	
 	public void m3(){
 		System.out.println("call m3 in test 3");
+	}
+	public void m4(){
+		System.out.println("m4 from test3");
 	}
 }
